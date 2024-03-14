c69eb548dcf1ac7df3b8425ff3f2e9b85a1ffea7
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2024-03-14 10:41:05 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,8 +7,6 @@ use Filament\Forms\FormsServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\View;
 use Livewire\LivewireServiceProvider;
@@ -20,27 +18,38 @@ class TestCase extends Orchestra
 {
 	use RefreshDatabase;
 	
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(fn (string $modelName) => 'RalphJSmit\\Filament\\SEO\\Database\\Factories\\' . class_basename($modelName) . 'Factory');
-	    
-	    View::addLocation(__DIR__ . '/Fixtures/resources/views');
+	protected function setUp(): void
+	{
+		parent::setUp();
 		
-		(include __DIR__ . '/Fixtures/migrations/create_test_tables.php')->up();
-		(include __DIR__ . '/../vendor/ralphjsmit/laravel-seo/database/migrations/create_seo_table.php.stub')->up();
+		Factory::guessFactoryNamesUsing(fn(string $modelName) => 'RalphJSmit\\Filament\\SEO\\Database\\Factories\\' . class_basename($modelName) . 'Factory');
+		
+		if ( version_compare(Application::VERSION, '11', '>=') ) {
+			View::addLocation(__DIR__ . '/Fixtures/resources/views');
+			
+			(include __DIR__ . '/Fixtures/migrations/create_test_tables.php')->up();
+			(include __DIR__ . '/../vendor/ralphjsmit/laravel-seo/database/migrations/create_seo_table.php.stub')->up();
+		}
 	}
-
-    protected function getPackageProviders($app): array
-    {
-        return [
-            LivewireServiceProvider::class,
-            FilamentServiceProvider::class,
-            FormsServiceProvider::class,
-            LaravelSEOServiceProvider::class,
-            FilamentSEOServiceProvider::class,
-            SupportServiceProvider::class,
-        ];
-    }
+	
+	protected function getPackageProviders($app): array
+	{
+		return [
+			LivewireServiceProvider::class,
+			FilamentServiceProvider::class,
+			FormsServiceProvider::class,
+			LaravelSEOServiceProvider::class,
+			FilamentSEOServiceProvider::class,
+			SupportServiceProvider::class,
+		];
+	}
+	
+	protected function getEnvironmentSetUp($app)
+	{
+		// La
+		if ( version_compare(Application::VERSION, '11', '>=') ) {
+			(include __DIR__ . '/Fixtures/migrations/create_test_tables.php')->up();
+			(include __DIR__ . '/../vendor/ralphjsmit/laravel-seo/database/migrations/create_seo_table.php.stub')->up();
+		}
+	}
 }
