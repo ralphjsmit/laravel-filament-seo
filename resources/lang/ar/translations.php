f7b70d71826347c3f7c4e5908e036c9bb3f03339
A909M <assemcc2022@gmail.com>
2025-01-26 05:12:14 +0300

    Add Arabic translations

---
+++ resources/lang/ar/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'العنوان',
+    'description' => 'الوصف',
+    'author' => 'اسم المؤلف',
+    'image' => 'صورة',
+    'characters' => 'الأحرف',
+    'robots' => 'وسم ميتا Robots',
+];
