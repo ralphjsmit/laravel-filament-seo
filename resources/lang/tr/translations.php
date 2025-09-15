e4929b45d408ea5fafce6203fc0b22bbcecbd35c
vedatyilmaz <veyilmaz@gmail.com>
2025-09-15 15:44:48 +0300

    turkish translation

---
+++ resources/lang/tr/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Başlık',
+    'description' => 'Açıklama',
+    'author' => 'Yazar Adı',
+    'image' => 'Görsel',
+    'characters' => 'Karakter',
+    'robots' => 'Robot meta etiketi',
+];
