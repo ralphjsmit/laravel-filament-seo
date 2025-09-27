e2c5349f9f260c831299a19730768bd8896b807e
Umer Farooq <zerokool_007@yahoo.com>
2025-09-27 18:36:25 +0400

    chore: upgrade to Pest v4 and Urdu translations (#47)
    * chore: update composer.json formatting and pestphp dependencies
    
    * add urdu translations for basic fields
    
    * Fix tests
    
    * Update composer.json
    
    ---------
    
    Co-authored-by: Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>

---
+++ resources/lang/ur/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'عنوان',
+    'description' => 'تفصیل',
+    'author' => 'مصنف کا نام',
+    'image' => 'تصویر',
+    'characters' => 'حروف',
+    'robots' => 'میٹا ٹیگ روبوٹس',
+];
